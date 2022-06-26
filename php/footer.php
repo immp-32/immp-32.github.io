77ebd7814a3478ddcb8c9172e25bc6db6d011c5f
immp-32 <immp3353@gmail.com>
2022-06-26 23:03:21 +0545

    first commit

--- php/footer.php
+++
@@ -1,9 +0,0 @@
-
-<div class="footer">
-<div class= "wrapper">
-    <p class= "text-center"> 2022 All Right Reserverd. Developed By Mahesh Pela And Siddhartha Bajracharya </p>
-</div>
-</div>
-
-</body>
-</html>
--- php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('../php/constants.php');
-session_destroy();
-
-header('location:'.SITEURL.'php/login.php');
-
-?>
